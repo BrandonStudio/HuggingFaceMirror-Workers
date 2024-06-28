868f643fd460da8a303b43d0af6e58954dec541b
BrandonStudio <brandonstudio@outlook.com>
2024-06-28 14:26:11 +0800

    version 1

--- src/index.ts
+++ src/index.ts
@@ -16,49 +16,84 @@ const UpstreamHost = 'huggingface.co';
 
 // Export a default object containing event handlers
 export default {
-  // The fetch handler is invoked when this worker receives a HTTP(S) request
-  // and should return a Response (optionally wrapped in a Promise)
-  async fetch(request, env, ctx): Promise<Response> {
-    // You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
-    const url = new URL(request.url);
-    const hostname = url.hostname;
-    const headers = new Headers(request.headers);
+	// The fetch handler is invoked when this worker receives a HTTP(S) request
+	// and should return a Response (optionally wrapped in a Promise)
+	async fetch(request, env, ctx): Promise<Response> {
+		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
+		const url = new URL(request.url);
+		const hostname = url.hostname;
+		const headers = new Headers(request.headers);
 
-    // Handle bad bots
-    if (/SemrushBot|MJ12bot/.test(headers.get('User-Agent') || '')) {
-      return new Response('Forbidden', { status: 403 });
-    }
+		// Handle bad bots
+		if (/SemrushBot|MJ12bot/.test(headers.get('User-Agent') || '')) {
+			return new Response('Forbidden', { status: 403 });
+		}
 
-    let request_to_upstream: Request;
-    if (hostname.startsWith(LfsPrefix)) {
-      // Handle lfs requests
+		let request_to_upstream: Request;
+		if (hostname.startsWith(LfsPrefix)) {
+			// Handle lfs requests
 
-      let cdn_prefix = hostname.split('.')[0];
-      let upstream_hostname = `${cdn_prefix}.${UpstreamHost}`;
-      let request_to_upstream_url = new URL(url);
-      request_to_upstream_url.hostname = upstream_hostname;
+			let cdn_prefix = hostname.split('.')[0];
+			let upstream_hostname = `${cdn_prefix}.${UpstreamHost}`;
+			let request_to_upstream_url = new URL(url);
+			request_to_upstream_url.hostname = upstream_hostname;
 
-      request_to_upstream = new Request(request_to_upstream_url, {
-        headers: request.headers,
-        method: request.method,
-        body: request.body,
-      });
-      request_to_upstream.headers.set('Host', upstream_hostname);
-    } else {
-      // Handle root requests
+			request_to_upstream = new Request(request_to_upstream_url, {
+				headers: request.headers,
+				method: request.method,
+				body: request.body,
+			});
+			request_to_upstream.headers.set('Host', upstream_hostname);
 
-      let request_to_upstream_url = new URL(url);
-      request_to_upstream_url.hostname = UpstreamHost;
+			const response = await fetch(request_to_upstream);
+			return response;
+		} else {
+			// Handle root requests
 
-      request_to_upstream = new Request(request_to_upstream_url, {
-        headers: request.headers,
-        method: request.method,
-        body: request.body,
-      });
-      request_to_upstream.headers.set('Host', UpstreamHost);
-    }
+			let request_to_upstream_url = new URL(url);
+			request_to_upstream_url.hostname = UpstreamHost;
 
-    const response = await fetch(request_to_upstream);
-    return response;
-  },
+			request_to_upstream = new Request(request_to_upstream_url, {
+				redirect: 'manual', // Prevent auto re-execution
+				headers: request.headers,
+				method: request.method,
+				body: request.body,
+			});
+			request_to_upstream.headers.set('Host', UpstreamHost);
+			request_to_upstream.headers.set('Accept-Encoding', 'identity'); // Get Content-Length header
+
+			const response = await fetch(request_to_upstream);
+
+			if (response.status === 301 || response.status === 302) {
+				// redirection to cdn-lfs
+				const location = response.headers.get('Location')!; // Location header should exists
+				let location_url = new URL(location);
+				const location_url_hostname = location_url.hostname;
+				const cdn_prefix = location_url_hostname.split('.')[0];
+				const new_hostname = `${cdn_prefix}.${hostname}`;
+				location_url.hostname = new_hostname;
+
+				const headers = new Headers(response.headers);
+				headers.set('Location', location_url.toString());
+
+				return new Response(response.body, {
+					status: response.status,
+					statusText: response.statusText,
+					headers: headers,
+				});
+			} else {
+				// normal response
+				const l = response.headers.get('Content-Length');
+
+				return new Response(response.body, {
+					status: response.status,
+					statusText: response.statusText,
+					headers: {
+						...response.headers,
+						'X-Linked-Size': l || '-1', // Use compatible X-Linked-Size header for transformers, as Content-Length will be removed.
+					},
+				});
+			}
+		}
+	},
 } satisfies ExportedHandler<Env>;
